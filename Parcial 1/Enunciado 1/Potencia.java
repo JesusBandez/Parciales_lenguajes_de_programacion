ed626a879fef112eb60f803640e2eac3b47f3471
JesusBandez <57400866+JesusBandez@users.noreply.github.com>
2022-02-24 12:47:12 -0400

    Agregados comentarios y el Readme

--- Parcial 1/Enunciado 1/Potencia.java
+++ Parcial 1/Enunciado 1/Potencia.java
@@ -4,31 +4,58 @@ recursiva.*/
 
 public class Potencia {
 
+// Implementacion recursiva de la funcion potencia
 public static int potenciarRecursivo(int a, int b) {
+    // Se asume que a y b son numeros no negativos.
+
+    // Si a y b son ambos 0, se arroja una exception
+    if (a == 0 && b == 0){
+        throw new RuntimeException("La potencia de 0 a la 0 no esta definida");
+    } else if (a<0 || b<0){
+        throw new RuntimeException("La base y el exponente deben ser no negativos");
+    }
+
+    // Si el exponente es 0, se retorna 1
     if (b==0){
         return 1;
+    
+    // Si el exponente no es 0, entonces se hace el paso recursivo
     } else {
         return a * potenciarRecursivo(a, b-1);
     }
 }
 
+// Implementacion iterativa de la funcion potencia
 public static int potenciarIterativo(int a, int b) {
+    // Se asume que a y b son numeros no negativos.
+
+    // Si a y b son ambos 0, se arroja una exception
+    if (a == 0 && b == 0){
+        throw new RuntimeException("La potencia de 0 a la 0 no esta definida");
+    } else if (a<0 || b<0){
+        throw new RuntimeException("La base y el exponente deben ser no negativos");
+    }
+
+    // variable acumulador que retorna el valor calculado
     int acc = 1;
+
+    // Mientras el exponente se mayor a 0, se multiplica
+    // el acumulador por la base y se reduce el valor de 
+    // b
     while (b > 0){
         acc = acc*a;
         b--;
     }
+
     return acc;
 }
 public static void main(String[] args) {
     // Se imprimen por salida estandar las respuestas de ambas
     // funciones para comprobar que esten correctas
     int base, exponente;
-    base = 4;
-    exponente = 3;
+    base = 4; exponente = 3;   
+
     System.out.println(potenciarRecursivo(base, exponente));
     System.out.println(potenciarIterativo(base, exponente));
-
-
 }
 }
---
+++ Parcial 1/Enunciado 1/README.md
@@ -0,0 +1,20 @@
+# Codigos del enunciado 1
+ En este directorio se encuentran los codigos que corresponden a los programas Potencia y Producto de matriz programados en Java.
+
+Los codigos fuentes se encuentran en los archivos de extension .Java y el codigo
+ya compilado se encuentra en los archivos de extension .class
+
+Para ejecutar un programa, necesita tener instalado java. Luego, ejecuta el siguiente comando:
+```
+java Potencia
+java ProductoMatriz
+```
+
+Si desea modificar alguno de los programas, puede cambiar el codigo fuente y compilarlos. Para ello
+debe descargar el JDK disponible en la página web de [Oracle](https://www.oracle.com/java/technologies/downloads/).
+Luego, los compila ejecutando:
+```
+javac Potencia.java
+javac ProductoMatriz.class
+```
+y finalmente los ejecuta con el comando `java`
